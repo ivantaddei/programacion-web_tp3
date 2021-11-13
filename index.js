9d3ccbeb8b42da412816d3a0bdf9c37d9364b1e4
Ivan Taddei <ivanarturotaddei@gmail.com>
2021-11-13 17:33:11 -0300

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const app = require('./src/app')
+require("dotenv").config();
+const app = require("./src/app");
 
-const PORT = process.env.HTTP_PORT || 3000
+const PORT = process.env.HTTP_PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server corriendo en http://localhost:${PORT}/`)
-})
+app.listen(PORT, () => {
+  console.log(`Server corriendo en http://localhost:${PORT}/`);
+});
